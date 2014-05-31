316dd849fcb58b78fe50d91565b60f0a9fdcffc2
Jordan Dick <jchan1e@gmail.com>
2014-05-31 16:54:17 -0600

    weighted randomizer between Quicksort, Mergesort, and Heapsort

--- quicksort.cxx
+++ quicksort.cxx
@@ -12,9 +12,11 @@
 #include <cmath>
 #include <iostream>
 #include <SFML/graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
 using namespace std;
 
-const int W = 1680;	// window width
+const int W = 1600;	// window width
 const int S = 1000;	// window height
 const int Q = 1;	// bar width
 const int N = W/Q;	// array size
@@ -32,6 +34,13 @@ void swap(int arr[], int a, int b);
 int parent(int child);
 int left_child(int parent);
 int right_child(int parent);
+void heapsort2(int arr[], int lo, int hi);
+void shift2(int arr[], int lo, int mid, int hi);
+void heapify2(int arr[], int lo, int hi);
+void swap2(int arr[], int a, int b);
+int parent2(int child, int hi);
+int left_child2(int parent2, int hi);
+int right_child2(int parent2, int hi);
 
 sf::RenderWindow window(sf::VideoMode(W, S), "Quicksort");
 
@@ -39,7 +48,7 @@ int main()
 {
 	//window = sf::RenderWindow(sf::VideoMode(W, S+5), "Quicksort");
 	
-	
+	srand(time(NULL));
 
 	//window.setFramerateLimit(300);
 
@@ -54,54 +63,173 @@ int main()
 	for(i = 0; i < N; ++i)
 		array[i] = pixel(i, 0, N, 0, MAX);
 
-	for(i = 0; i < N; ++i)
-	{
-		temp = array[i];
-		n = rand() %N;
-		array[i] = array[n];
-		array[n] = temp;
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
 
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	} 
-	
-	quicksort(array, 0, N-1);
+while(true)
+{
+	i = rand()%12;
 
-// triangular array
-	for(i = 0; i < N/2; ++i)
+	if (i == 0 || i == 1 || i == 11)
 	{
-		array[i] = pixel(i, N/2, 0, MAX, 0);
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
+		//window.setFramerateLimit(120);
+		for(i = 0; i < N; ++i)
+		{
+			temp = array[i];
+			n = rand() %N;
+			array[i] = array[n];
+			array[n] = temp;
+
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		} 
+
+		quicksort(array, 0, N-1);
 	}
-	for(i = N/2; i < N; ++i)
-	{
-		array[i] = pixel(i, N, N/2, 0, MAX)+1;
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	} 
 
-	quicksort(array, 0, N-1);
+// triangular arrays
+	else if (i == 2)
+	{
+		//window.setFramerateLimit(120);
+		for(i = 0; i < N/2; ++i)
+		{
+			array[i] = pixel(i, 0, N/2, 0, MAX);
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		}
+		for(i = N/2; i < N; ++i)
+		{
+			array[i] = pixel(i, N/2, N, MAX, 0)+1;
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		} 
+
+		quicksort(array, 0, N-1);
+	}
+	else if (i == 3)
+	{
+		//window.setFramerateLimit(120);
+		for(i = 0; i < N/2; ++i)
+		{
+			array[i] = pixel(i, 0, N/2, MAX, 0);
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		}
+		for(i = N/2; i < N; ++i)
+		{
+			array[i] = pixel(i, N/2, N, 0, MAX)+1;
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		} 
+
+		quicksort(array, 0, N-1);
+	}
 
 // randomized array
-/*	for(i=0; i < N; ++i)
+	else if (i == 4)
 	{
-		temp = array[i];
-		n = rand() %N;
-		array[i] = array[n];
-		array[n] = temp;
+		//window.setFramerateLimit(180);
+		for(i = 0; i < N; ++i)
+		{
+			temp = array[i];
+			n = rand() %N;
+			array[i] = array[n];
+			array[n] = temp;
 
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	} 
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		} 
 
-	mergesort(array, 0, N-1); */
+		mergesort(array, 0, N-1); 
+	}
 
 // reversed array
+	else if (i == 5)
+	{
+		//window.setFramerateLimit(0)
+	;	for(i = 0; i < N; ++i)
+		{
+			array[i] = pixel(i, 0, N, MAX, 0);
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		}
+
+		mergesort(array, 0, N-1);
+	}
+
+// randomized array
+	else if (i == 6 || i == 7)
+	{
+		//window.setFramerateLimit(180);
+		for(i = 0; i < N; ++i)
+		{
+			temp = array[i];
+			n = rand() %N;
+			array[i] = array[n];
+			array[n] = temp;
+
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		}
+
+		heapsort(array, 0, N-1);
+	}
+
+// reverse sorted array
+	else if (i == 8)
+	{
+		//window.setFramerateLimit(240);
+		for(i = 0; i < N; ++i)
+		{
+			array[i] = pixel(i, 0, N, MAX, 0);
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		}
+
+		heapsort(array, 0, N-1);
+	}
+
+// triangular array
+	else if (i == 9)
+	{
+		//window.setFramerateLimit(240);
+		for(i = 0; i < N/2; ++i)
+		{
+			array[i] = pixel(i, N/2, 0, MAX, 0);
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		}
+		for(i = N/2; i < N; ++i)
+		{
+			array[i] = pixel(i, N, N/2, 0, MAX)+1;
+			window.clear(sf::Color::Black);
+			draw_array(N, array, MAX);
+			window.display();
+		}
+
+		heapsort(array, 0, N-1);
+	}
+
+// sorted array
+	else if (i == 10)
+	{
+		//window.setFramerateLimit(240);
+		heapsort(array, 0, N-1);
+	}
+
+}
+
+// BOGO sort!
 /*	for(i = 0; i < N; ++i)
 	{
 		array[i] = pixel(i, 0, N, MAX, 0);
@@ -110,63 +238,8 @@ int main()
 		window.display();
 	}
 
-	mergesort(array, 0, N-1);*/
-
-// randomized array
-	for(i = 0; i < N; ++i)
-	{
-		temp = array[i];
-		n = rand() %N;
-		array[i] = array[n];
-		array[n] = temp;
-
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	}
-
-	heapsort(array, 0, N-1);
-
-// reverse sorted array
-	for(i = 0; i < N; ++i)
-	{
-		array[i] = pixel(i, 0, N, MAX, 0);
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	}
-
-	heapsort(array, 0, N-1);
-
-// triangular array
-	for(i = 0; i < N/2; ++i)
-	{
-		array[i] = pixel(i, N/2, 0, MAX, 0);
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	}
-	for(i = N/2; i < N; ++i)
-	{
-		array[i] = pixel(i, N, N/2, 0, MAX)+1;
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	}
-
-	heapsort(array, 0, N-1);
-
-// BOGO sort!
-	for(i = 0; i < N; ++i)
-	{
-		array[i] = pixel(i, 0, N, MAX, 0);
-		window.clear(sf::Color::Black);
-		draw_array(N, array, MAX);
-		window.display();
-	}
-
-	window.setFramerateLimit(0);
-
+	//window.setFramerateLimit(0);
+;
 	bool sorted = false;
 	for (int i = 0; array[i] < array[i+1] && i < N-1; ++i)
 		{
@@ -206,7 +279,7 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-	}
+	}*/
 
 	return EXIT_SUCCESS;
 }
@@ -224,11 +297,17 @@ void quicksort(int arr[], int left, int right)
 	}
 
 	int i = left, j = right;
-	int tmp;
-	int pivot = arr[(left + right) / 2];
+	int tmp1 = arr[left], tmp2 = arr[(left+right)/2], tmp3 = arr[right];
+	int pivot;
+	if (tmp1 <= tmp2 && tmp2 < tmp3)
+		pivot = tmp2;
+	else if (tmp1 <= tmp3 && tmp3 < tmp2)
+		pivot = tmp3;
+	else
+		pivot = tmp1;
 	
 	// partition
-	while (i <= j)
+	while (i < j)
 	{
 		while (arr[i] < pivot)
 			++i;
@@ -244,19 +323,27 @@ void quicksort(int arr[], int left, int right)
 //		swap
 		if (i <= j)
 		{
-			tmp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = tmp;
+			swap(arr, i, j);
 			++i;
 			--j;
 		}
 	}
 	
 	// recursion
-	if (left < j)
-		quicksort(arr, left, j);
-	if (i < right)
-		quicksort(arr, i, right);
+	/*if (rand() %2 == 1)
+	{
+		if (left < j)
+			quicksort(arr, left, j);
+		if (i < right)
+			quicksort(arr, i, right);
+	}
+	else
+	{*/
+		if (i < right)
+			quicksort(arr, i, right);
+		if (left < j)
+			quicksort(arr, left, j);
+	//}
 }
 
 void mergesort(int arr[], int lo, int hi)
@@ -418,7 +505,7 @@ void shift(int arr[], int lo, int hi)
 
 void heapify(int arr[], int lo, int hi)
 {
-	for(int mid = (hi-lo-1/2); mid >= 0; --mid)
+	for(int mid = (hi-lo-1)/2; mid >= 0; --mid)
 	{
 		shift(arr, mid, hi);
 		window.clear();
@@ -434,9 +521,86 @@ void swap(int arr[], int a, int b)
 	arr[b] = tmp;
 }
 
+void shift2(int arr[], int lo, int mid, int hi)
+{
+	int a = mid;
+	int b = a;
+	while (left_child2(a, hi) >= lo)
+	{
+		if (arr[a] < arr[left_child2(a, hi)])
+			b = left_child2(a, hi);
+		if (right_child2(a, hi) >= lo && arr[b] < arr[right_child2(a, hi)])
+			b = right_child2(a, hi);
+		if (b != a)
+		{
+			swap(arr, a, b);
+			//lines(a, b);
+			a = b;
+			//window.clear();
+			//draw_array(N, arr, MAX);
+			//window.display();
+		}
+		else
+			break;
+	}
+	//window.clear();
+	//draw_array(N, arr, MAX);
+	//window.display();
+	return;
+}
+
+void heapify2(int arr[], int lo, int hi)
+{
+	for(int mid = (hi-lo)/2; mid <= hi; ++mid)
+	{
+		shift2(arr, lo, mid, hi);
+		window.clear();
+		draw_array(N, arr, MAX);
+		window.display();
+	}
+}
+
+void heapsort2(int arr[], int lo, int hi)
+{
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		// "close requested" event: we close the window
+		if (event.type == sf::Event::Closed) {
+			window.close();
+			exit(0);
+		}
+	}
+
+	heapify2(arr, lo, hi);
+	for(int i = hi; i > lo; --i)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+		// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed) {
+				window.close();
+				exit(0);
+			}
+		}
+
+		heapify2(arr, lo, i);
+		//shift2(arr, lo, i, i);
+		//swap(arr, 0, i);
+		//window.clear();
+		//draw_array(N, arr, MAX);
+		//window.display();
+	}
+}
+
 int parent(int child) {return (child - 1)/2;}
 int left_child(int parent) {return 2*parent + 1;}
 int right_child(int parent) {return 2*parent + 2;}
+
+int parent2(int child, int hi) {return child+1+(hi-child)/2;}
+int left_child2(int parent2, int hi) {return hi-2*(hi-parent2) - 1;}
+int right_child2(int parent2, int hi) {return hi-2*(hi-parent2) - 2;}
 
 void draw_array(int n, int arr[], int max)
 {
@@ -459,19 +623,22 @@ void draw_array(int n, int arr[], int max)
 
 	sf::Vertex line0[] =
 	{	sf::Vertex(sf::Vector2f(0, 0)),
-		sf::Vertex(sf::Vector2f(0, 0))  };
+		sf::Vertex(sf::Vector2f(0, 0)),
+		/*sf::Vertex(sf::Vector2f(0, 0))*/  };
 
 
 	for(i = 0; i < n; ++i)
 	{
 		line0[0] = sf::Vertex(sf::Vector2f(Q*i, S));
-		line0[1] = sf::Vertex(sf::Vector2f(Q*i, pixel(double(arr[i]), double(0), double(max), S, 0)));
-		line0[0].color = sf::Color::Black;
+		line0[1] = sf::Vertex(sf::Vector2f(Q*i, S-arr[i]));
+		//line0[2] = sf::Vertex(sf::Vector2f(Q*i, 0));
 		line0[1].color = sf::Color::Color (
 			min(abs(std::max(abs(((2*arr[i]+512) %1536)-768)-256,0)),255),
 			min(abs(std::max(abs(((2*arr[i]) %1536)-768)-256,0)),255),
 			min(abs(std::max(abs(((2*arr[i]-512) %1536)-768)-256,0)),255));
-		window.draw(line0, 2, sf::Lines);
+		line0[0].color = sf::Color::Black;
+		//line0[2].color = line0[0].color;
+		window.draw(line0, 2, sf::Lines);//Strip);
 	}
 }
 
